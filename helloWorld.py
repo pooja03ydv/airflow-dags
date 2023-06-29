273d55da205321a5cb35db7d94441f8ed933a7f2
pooja03ydv <36677428+pooja03ydv@users.noreply.github.com>
2023-06-29 11:31:35 +0530

    Update helloWorld.py

--- helloWorld.py
+++ helloWorld.py
@@ -8,7 +8,7 @@ def print_hello():
     print('Hello world from first Airflow DAG!')
 
 dag = DAG('hello_world', description='Hello World DAG',
-          schedule=''@daily',
+          schedule='@daily',
           start_date=datetime(2017, 3, 20), catchup=False)
 
 hello_operator = PythonOperator(task_id='hello_k8s', python_callable=print_hello, dag=dag)
