337b8e83dcc7abce8e39b8db45ec699eff60bc06
pooja03ydv <36677428+pooja03ydv@users.noreply.github.com>
2023-06-30 13:55:01 +0530

    Update resource.py

--- resource.py
+++ resource.py
@@ -1,48 +1,34 @@
-from __future__ import annotations
-import logging
-import os
-import pendulum
-from airflow.configuration import conf
-from airflow.decorators import task
-from airflow.example_dags.libs.helper import print_stuff
-from datetime import datetime
 from airflow import DAG
-from airflow.operators.dummy_operator import DummyOperator
-from airflow.operators.python_operator import PythonOperator
-from airflow.operators.bash_operator import BashOperator
-from kubernetes import client as k8s
+from airflow.operators.python import PythonOperator
+from airflow.utils.dates import days_ago
+from airflow.models import Variable
+from modelTestCI.dags.helper_functions.cpu_stress_time import execute
 
-with DAG(
-    dag_id="hello_world",
-    schedule_interval='@daily',
-    start_date=datetime(2017, 3, 20), 
-    catchup=False,
-    tags=["example3"],
-) as dag:
+default_args = {
+    'owner': 'Felipe Test',
+    'depends_on_past': False,
+    'start_date': days_ago(0), 
+    'email': ['airflow@example.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 2,
+}
 
-    # Use k8s_client.V1ResourceRequirements to define resource limits
-    k8s_resource_requirements = k8s.V1ResourceRequirements(
-        requests={"memory": "512Mi"}, limits={"memory": "512Mi"}
+resource_config = {"KubernetesExecutor": {"request_memory": "200Mi", 
+                                          "limit_memory": "200Mi", 
+                                          "request_cpu": "200m", 
+                                          "limit_cpu": "200m"}}
+
+with DAG(dag_id='std_simple_sample', schedule_interval=None, 
+         tags=['analytics'], default_args=default_args) as dag:
+    
+    task = PythonOperator(
+        task_id='std_simple_sample_task',
+        python_callable=execute,
+        op_args=[20,timetask],
+        start_date=days_ago(0),
+        owner='airflow',
+        executor_config = resource_config
     )
 
-    kube_exec_config_resource_limits = {
-        "pod_override": k8s.V1Pod(
-            spec=k8s.V1PodSpec(
-                containers=[
-                    k8s.V1Container(
-                        name="base",
-                        resources=k8s_resource_requirements,
-                    )
-                ],
-                affinity=k8s_affinity,
-                tolerations=k8s_tolerations,
-            )
-        )
-    }
-
-    @task(executor_config=kube_exec_config_resource_limits)
-    def task_with_resource_limits():
-        return BashOperator(task_id='sleep', bash_command='sleep 300')
-
-    four_task = task_with_resource_limits()
-    four_task >> DummyOperator(task_id="dummy_task")
+    task
