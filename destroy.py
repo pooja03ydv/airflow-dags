17a834d4f497d66364758115e89a33cb6aef18f4
pooja03ydv <36677428+pooja03ydv@users.noreply.github.com>
2023-07-04 14:12:20 +0530

    Create destroy.py

---
+++ destroy.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+from airflow.models import DAG
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.python_operator import PythonOperator
+
+
+def print_hello():
+    print('Hello world from first Airflow DAG!')
+
+dag = DAG('destroy', description='Hello World DAG',
+          schedule='@daily',
+          start_date=datetime(2017, 3, 20), catchup=False)
+
+hello_operator = PythonOperator(task_id='hello_k8s', python_callable=print_hello, dag=dag)
+
+hello_operator
+dag.destroy()
