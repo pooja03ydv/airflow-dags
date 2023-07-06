2ef2f24384fefbda23958860c35ddfb5a29b7d01
pooja03ydv <36677428+pooja03ydv@users.noreply.github.com>
2023-07-06 17:40:07 +0530

    Update executor.py

--- executor.py
+++ executor.py
@@ -29,6 +29,7 @@ from airflow import DAG
 from airflow.configuration import conf
 from airflow.decorators import task
 from airflow.example_dags.libs.helper import print_stuff
+from airflow.operators.python_operator import PythonOperator
 
 log = logging.getLogger(__name__)
 
