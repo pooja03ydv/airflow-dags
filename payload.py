6cabadb6fe3d244035b54786be9d63521d9b5108
pooja03ydv <36677428+pooja03ydv@users.noreply.github.com>
2023-07-07 15:55:02 +0530

    Update payload.py

--- payload.py
+++ payload.py
@@ -1,7 +1,6 @@
 from datetime import datetime
 from airflow import DAG
 from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
-from airflow.operators.python_operator import PythonOperator
 
 default_args = {
     'owner': 'airflow',
@@ -9,43 +8,16 @@ default_args = {
 }
 
 dag = DAG(
-    'create_pod_with_resource_limits',
-     default_args=default_args,
+    'start_kubernetes_pod',
+    default_args=default_args,
     schedule_interval=None
 )
 
-def create_pod(payload):
-    pod_manifest = {
-        "apiVersion": "v1",
-        "kind": "Pod",
-        "metadata": {
-            "name": "my-pod",
-            "labels": {
-                "app": "my-app"
-            }
-        },
-        "spec": {
-            "containers": [
-                {
-                    "name": "spring",
-                    "image": "poyadav3/mavenbuild:66",
-                    "resources": {
-                        "limits": {
-                            "cpu": "1",
-                            "memory": "1Gi"
-                        }
-                    }
-                }
-            ]
-        }
-    }
-
-    api_response = client.CoreV1Api().create_namespaced_pod(body=pod_manifest, namespace="default")
-    print(api_response)
-
-create_pod_task = PythonOperator(
-    task_id='create_pod',
-    python_callable=create_pod,
-    op_kwargs={'payload': '{{ dag_run.conf }}'},
+start_pod_task = KubernetesPodOperator(
+    task_id='start_pod',
+    name='my-pod',
+    image='poyadav3/mavenbuild:66',
+    namespace='default',
+    get_logs=True,
     dag=dag
 )
