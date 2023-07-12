18dcb5376b6dbf78b0e8f2f695b593bd49085f6e
pooja03ydv <36677428+pooja03ydv@users.noreply.github.com>
2023-07-12 10:54:32 +0530

    Update payload.py

--- payload.py
+++ payload.py
@@ -9,24 +9,25 @@ default_args = {
 }
 
 dag = DAG(
-    dag_id="start_kubernetes_pod",
+    'start_kubernetes_pod',
     default_args=default_args,
     schedule_interval=None
 )
 
-def start_pod():
+def start_pod(resource_limits):
     start_pod_task = KubernetesPodOperator(
         task_id='start_pod',
         name='my-pod',
         image='poyadav3/mavenbuild:66',
         namespace='default',
         get_logs=True,
+        resources=resource_limits,
         dag=dag
-     )
-
+    )
 
 start_pod_task = PythonOperator(
     task_id='start_pod_task',
     python_callable=start_pod,
+    op_args=[{'cpu': '1', 'memory': '1Gi'}],
     dag=dag
 )
